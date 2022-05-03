00b0082481528c1afa31387ff52d810aa6afde89
ryannguyen0303 <ryannguyen0303@gmail.com>
2022-05-03 17:41:21 +0700

    chore: refactor homepage route

---
+++ components/Homepage/Homepage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header/Header";
+import Features from "./Features/Features";
+import HowItWork from "./How it work/HowItWork";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <Features />
+      <HowItWork />
+    </>
+  );
+}
+
+export default Homepage;
