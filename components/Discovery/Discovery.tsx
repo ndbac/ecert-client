9728158ee0ebd9b87a546a225cf493546c1529ef
ryannguyen0303 <ryannguyen0303@gmail.com>
2022-05-07 16:20:24 +0700

    feat: create dark theme for discovery page

--- components/Discovery/Discovery.tsx
+++ components/Discovery/Discovery.tsx
@@ -1,225 +1,58 @@
 /* eslint-disable react/no-unescaped-entities */
-/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
+
+import ShadowBlueFull from "../../public/elements/shadow-blue-full.svg";
+import ShadowLight7 from "../../public/elements/shadow-light-7.svg";
+// import BlueDot from "../../public/icons/dots/blue-dot-left-bars.svg";
+// import YellowDot from "../../public/icons/dots/yellow-dot-right-shield.svg";
+
+import Postcard from './Postcard/Postcard';
 
 function Discovery() {
   return (
-    <section className="relative py-20">
-      <img
-        className="hidden lg:block absolute top-0 left-0 mt-20"
-        src="zeus-assets/icons/dots/blue-dot-left-bars.svg"
-        alt=""
-      />
-      <img
-        className="hidden lg:block absolute top-0 right-0 mt-52"
-        src="zeus-assets/icons/dots/yellow-dot-right-shield.svg"
-        alt=""
-      />
+    <section className="relative py-20 bg-black">
+      <div className="absolute top-0 left-0 -ml-40">
+        <Image src={ShadowBlueFull} alt="" />
+      </div>
+      <div className="hidden sm:block absolute top-1/2 right-0 -mr-52 transform -translate-y-1/2 object-contain">
+        <Image src={ShadowLight7} alt="" />
+      </div>
+      {/* <div className="hidden lg:block absolute top-0 left-0 mt-20">
+        <Image
+          src={BlueDot}
+          alt=""
+        />
+      </div>
+      <div className="hidden lg:block absolute top-0 right-0 mt-52">
+        <Image
+          src={YellowDot}
+          alt=""
+        />
+      </div> */}
       <div className="container px-4 mx-auto">
         <div className="max-w-3xl mb-20 mx-auto text-center">
-          <span className="text-xs text-blue-400 font-semibold">
+          <span className="text-xl text-yellow-400 font-semibold">
             What's new at Shuffle
           </span>
-          <h2 className="mt-8 mb-10 text-4xl font-semibold font-heading">
+          <h2 className="text-white mt-8 mb-10 text-4xl font-semibold font-heading">
             Lorem ipsum dolor sit amet consectutar domor at elis
           </h2>
-          <p className="text-xl text-gray-500">
+          <p className="text-xl text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
             aliquet orci.
           </p>
         </div>
         <div className="flex flex-wrap -m-3 mb-16">
-          <div className="w-full md:w-1/2 lg:w-1/3 p-3">
-            <div className="p-6 border rounded-xl">
-              <div className="relative h-52 mb-6">
-                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">
-                  Development
-                </span>
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src="https://images.unsplash.com/photo-1544717297-fa95b6ee9643?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </div>
-              <span className="inline-block mb-4 text-xs text-gray-500">
-                10 jun 2020 19:40
-              </span>
-              <h2 className="mb-4 text-2xl font-semibold font-heading">
-                Lorem ipsum dolor sit amet consectutar
-              </h2>
-              <p className="mb-4 text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
-                aliquet orci.
-              </p>
-              <a
-                className="text-lg font-medium text-red-500 underline hover:no-underline"
-                href="#"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/3 p-3">
-            <div className="p-6 border rounded-xl">
-              <div className="relative h-52 mb-6">
-                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">
-                  Development
-                </span>
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </div>
-              <span className="inline-block mb-4 text-xs text-gray-500">
-                10 jun 2020 19:40
-              </span>
-              <h2 className="mb-4 text-2xl font-semibold font-heading">
-                Lorem ipsum dolor sit amet consectutar
-              </h2>
-              <p className="mb-4 text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
-                aliquet orci.
-              </p>
-              <a
-                className="text-lg font-medium text-red-500 underline hover:no-underline"
-                href="#"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/3 p-3">
-            <div className="p-6 border rounded-xl">
-              <div className="relative h-52 mb-6">
-                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">
-                  Development
-                </span>
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </div>
-              <span className="inline-block mb-4 text-xs text-gray-500">
-                10 jun 2020 19:40
-              </span>
-              <h2 className="mb-4 text-2xl font-semibold font-heading">
-                Lorem ipsum dolor sit amet consectutar
-              </h2>
-              <p className="mb-4 text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
-                aliquet orci.
-              </p>
-              <a
-                className="text-lg font-medium text-red-500 underline hover:no-underline"
-                href="#"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/3 p-3">
-            <div className="p-6 border rounded-xl">
-              <div className="relative h-40 mb-6">
-                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">
-                  Development
-                </span>
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src="https://images.unsplash.com/photo-1557804506-d8017c1e4856?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1001&amp;q=80"
-                  alt=""
-                />
-              </div>
-              <span className="inline-block mb-4 text-xs text-gray-500">
-                10 jun 2020 19:40
-              </span>
-              <h2 className="mb-4 text-2xl font-semibold font-heading">
-                Lorem ipsum dolor sit amet consectutar
-              </h2>
-              <p className="mb-4 text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
-                aliquet orci.
-              </p>
-              <a
-                className="text-lg font-medium text-red-500 underline hover:no-underline"
-                href="#"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/3 p-3">
-            <div className="p-6 border rounded-xl">
-              <div className="relative h-40 mb-6">
-                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">
-                  Development
-                </span>
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </div>
-              <span className="inline-block mb-4 text-xs text-gray-500">
-                10 jun 2020 19:40
-              </span>
-              <h2 className="mb-4 text-2xl font-semibold font-heading">
-                Lorem ipsum dolor sit amet consectutar
-              </h2>
-              <p className="mb-4 text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
-                aliquet orci.
-              </p>
-              <a
-                className="text-lg font-medium text-red-500 underline hover:no-underline"
-                href="#"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 lg:w-1/3 p-3">
-            <div className="p-6 border rounded-xl">
-              <div className="relative h-40 mb-6">
-                <span className="absolute top-0 right-0 mt-4 mr-4 text-xs text-white px-2 py-1 font-semibold bg-gray-600 bg-opacity-50 rounded-md uppercase">
-                  Development
-                </span>
-                <img
-                  className="w-full h-full object-cover rounded-lg"
-                  src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </div>
-              <span className="inline-block mb-4 text-xs text-gray-500">
-                10 jun 2020 19:40
-              </span>
-              <h2 className="mb-4 text-2xl font-semibold font-heading">
-                Lorem ipsum dolor sit amet consectutar
-              </h2>
-              <p className="mb-4 text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan
-                aliquet orci.
-              </p>
-              <a
-                className="text-lg font-medium text-red-500 underline hover:no-underline"
-                href="#"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
+          <Postcard />
         </div>
         <div className="text-center">
           <a
-            className="px-6 py-4 text-sm text-white font-semibold bg-red-400 hover:bg-red-300 rounded transition duration-200"
+            className="px-6 py-4 text-sm font-semibold bg-yellow-400 hover:bg-yellow-500 rounded transition duration-200"
             href="#"
           >
             View More Articles
