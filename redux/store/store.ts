904d8f3add3bda491a67350bece6f9ac3daacb75
TienLePham <tienlepham094@gmail.com>
2022-05-23 20:32:29 +0700

    feat: create verify account

--- redux/store/store.ts
+++ redux/store/store.ts
@@ -5,14 +5,15 @@ import notiReducer from '../modules/notification/slices/notification.slice'
 import categoryReducer from '../modules/category/slices/category.slice'
 import commentReducer from '../modules/comment/slices/comment.slice'
 import accountReducer from '../modules/account/slices/account.slice'
-
+import verifyReducer from '../modules/verify/slices/verify.slice'
 const store = configureStore({
   reducer: {
     authReducer,
     notiReducer,
     categoryReducer,
     commentReducer,
-    accountReducer
+    accountReducer,
+    verifyReducer
   },
 })
 
